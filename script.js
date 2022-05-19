37bac5a2319b2040b901f71746a9823ba617c939
Leonardo Echegaray <leonvvalenzuela@gmail.com>
2022-05-18 21:12:34 -0500

    js final

--- script.js
+++ script.js
@@ -1,59 +1,83 @@
-'use strict'
-
-const listaPoke = document.getElementById ('listaPoke')
-const nombrePoke = document.getElementById ('nombre')
-const pokeNumber =150
+// 'use strict'
 
 
-const getPoke = () => {
-    const URL = `https://pokeapi.co/api/v2/pokemon`
-    fetch(URL)
-    
-    .then ((result) => result.json())
-    .then ((pokemons) => {
-    nombrePoke.innerHTML = pokemons.results[0].name
-        console.log("Soy un pokemon", pokemons);
-    })
+// const pikachu = document.getElementById ('1')
+// const id = document.getElementById('IDE')
+// const nombre = document.getElementById('NAME')
+// const moves = document.getElementById('MOVES')
+// const peso = document.getElementById('PESO')
 
-}
 
-getPoke()
-// const getPoke = () => {
-//     const URL = `https://pokeapi.co/api/v2/pokemon`
-//     fetch(URL)
+// const getPikachu = () => {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/`)
     
 //     .then ((result) => result.json())
 //     .then ((data) => {
-//         createPokemon(data);
-//     })
     
+//     id.innerHTML = `ID: ${JSON.stringify(data.id)}`
+//     nombre.innerHTML = ` ${JSON.stringify(data.name)}`
+//     moves.innerHTML = `Habilidad: ${JSON.stringify(data.abilities[0].ability.name)}`
+//     peso.innerHTML = `Peso: ${JSON.stringify(data.weight)} libras`
+//         console.log(data);
+//     })
+//     array.forEach(element => {
+        
+//     });
 // }
 
-
-// const createPokemon = (pokemon) => {
-//     const card = document.createElement ('div')
-//     card.classList.add('pokemon-block')
-
-//     const spriteContainer = document.createElement('div')
-//     spriteContainer.classList.add('img-container')
-
-//     const sprite = document.createElement('img')
-//     sprite.src = pokemon.sprites.front_default
-
-//     spriteContainer.appendChild(sprite)
-
-//     const number = document.createElement('p')
-//     number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`
-
-//     const name = document. createElement('p')
-//     name.classList.add('name')
-//     name.textContent = pokemon.name
-
-//     card.appendChild(spriteContainer)
-//     card.appendChild(number)
-//     card.appendChild(name)
-
-//     pokemonContainer.appendChild(card)
-// }
+// getPikachu()
 
 
+'use strict'
+
+
+const nombrePoke = document.getElementById ('nombre')
+
+
+
+const getPoke = (id) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    
+    .then ((result) => result.json())
+    .then ((data) => {
+        createPokemon(data)
+    });
+}
+
+function fetchPokemons(number){
+    for (let i = 1; i <= number; i++) {
+        getPoke(i)
+        
+    }
+}
+
+
+
+function createPokemon(pokemon){
+    const card = document.createElement ('div')
+    card.classList.add('pokemon-block')
+
+    const spriteContainer = document.createElement('div')
+    spriteContainer.classList.add('img-container')
+
+    const sprite = document.createElement('img')
+    sprite.src = pokemon.sprites.front_default
+
+    spriteContainer.appendChild(sprite)
+
+    const number = document.createElement('p')
+    number.textContent = `#${pokemon.id}`
+
+    const name = document.createElement('p')
+    name.classList.add('name')
+    name.textContent = pokemon.name
+
+    card.appendChild(spriteContainer)
+    card.appendChild(number)
+    card.appendChild(name)
+
+    nombrePoke.appendChild(card)
+
+}
+
+fetchPokemons(5)
