9d7817512d9009001596c8978da4ed6d2c88ead9
Leonardo Echegaray <leonvvalenzuela@gmail.com>
2022-05-19 11:41:31 -0500

    Primeros cambios con CSS

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+'use strict'
+const nombrePoke = document.getElementById ('nombre')
+
+const getPoke = (id) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    
+    .then ((result) => result.json())
+    .then ((data) => {
+        createPokemon(data)
+    });
+}
+
+function fetchPokemons(number){
+    for (let i = 1; i <= number; i++) {
+        getPoke(i)
+        
+    }
+}
+
+function createPokemon(pokemon){
+    const card = document.createElement ('div')
+    card.classList.add('pokemon-block')
+
+    const spriteContainer = document.createElement('div')
+    spriteContainer.classList.add('img-container')
+
+    const sprite = document.createElement('img')
+    sprite.src = pokemon.sprites.front_default
+
+    spriteContainer.appendChild(sprite)
+
+    const number = document.createElement('p')
+    number.classList.add('number')
+    number.textContent = `No. ${pokemon.id.toString().padStart(3, 0)}`
+
+    const name = document.createElement('p')
+    name.classList.add('name')
+    name.textContent = pokemon.name
+
+    const weight = document.createElement('p')
+    weight.classList.add('weight')
+    weight.textContent = `Peso: ${pokemon.weight} libras`
+
+    const abilities = document.createElement('p')
+    abilities.classList.add('abilities')
+    abilities.textContent = `Habilidad: ${pokemon.abilities[0].ability.name}`
+
+    card.appendChild(spriteContainer)
+    card.appendChild(number)
+    card.appendChild(name)
+    card.appendChild(weight)
+    card.appendChild(abilities)
+
+    nombrePoke.appendChild(card)
+}
+
+fetchPokemons(250)
+
+
+
+
+
+
 // 'use strict'
 
 
@@ -26,58 +89,3 @@
 // }
 
 // getPikachu()
-
-
-'use strict'
-
-
-const nombrePoke = document.getElementById ('nombre')
-
-
-
-const getPoke = (id) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    
-    .then ((result) => result.json())
-    .then ((data) => {
-        createPokemon(data)
-    });
-}
-
-function fetchPokemons(number){
-    for (let i = 1; i <= number; i++) {
-        getPoke(i)
-        
-    }
-}
-
-
-
-function createPokemon(pokemon){
-    const card = document.createElement ('div')
-    card.classList.add('pokemon-block')
-
-    const spriteContainer = document.createElement('div')
-    spriteContainer.classList.add('img-container')
-
-    const sprite = document.createElement('img')
-    sprite.src = pokemon.sprites.front_default
-
-    spriteContainer.appendChild(sprite)
-
-    const number = document.createElement('p')
-    number.textContent = `#${pokemon.id}`
-
-    const name = document.createElement('p')
-    name.classList.add('name')
-    name.textContent = pokemon.name
-
-    card.appendChild(spriteContainer)
-    card.appendChild(number)
-    card.appendChild(name)
-
-    nombrePoke.appendChild(card)
-
-}
-
-fetchPokemons(5)
